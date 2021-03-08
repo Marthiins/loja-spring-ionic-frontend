e327a338e05ef88b7e5f3d71e2cced54f98a0a2c
Marthiins <sergio.marthiins@gmail.com>
2021-03-07 22:03:17 -0300

    Apagando pagina list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+
+
+//App.component é o controaldor da pagina app.html
 
 @Component({
   templateUrl: 'app.html'
@@ -21,9 +23,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
-    ];
+      { title: 'Home', component: HomePage }
+     ];
 
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,15 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-@NgModule({
+@NgModule({ //Tudo que for declarado aqui temos que declarar em entryComponents
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -23,9 +22,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+
   ],
-  providers: [
+  providers: [//declar as classe dos objetos injetados
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
