7387889f4b5ae2d69d87fee63789e8b9030b9fe4
Marthiins <sergio.marthiins@gmail.com>
2021-03-09 18:31:37 -0300

    Extraindo o email do token e armazenando-o em localStorage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,12 +1,17 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { JwtHelperService } from "@auth0/angular-jwt";
 import { API_CONFIG } from "../config/api.config";
 import { CredenciaisDTO } from "../models/credenciais.dto";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
 
+
+
 @Injectable()
 export class AuthService {
+
+  jwtHelperService: JwtHelperService = new JwtHelperService(); //email para estrair do token dependencia
 
   constructor(public http: HttpClient, public storage: StorageService) { //requisição email e senha do meu back no andpoint login
   }
@@ -23,7 +28,8 @@ export class AuthService {
   successfulLogin(authorizationValue : string) {
     let tok = authorizationValue.substring(7); //tirar a palavra Barry do token reportar o string apartir do 7 caractere
     let user : LocalUser = {
-        token: tok
+        token: tok,
+        email: this.jwtHelperService.decodeToken(tok).sub
     };
     this.storage.setLocalUser(user);//Armazenar usuario no local store
 }
