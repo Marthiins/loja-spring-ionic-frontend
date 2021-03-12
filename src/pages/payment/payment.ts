5e024ba49ae69ca414695af1aedf32f85e18bbb4
Marthiins <sergio.marthiins@gmail.com>
2021-03-12 16:04:44 -0300

    Tela de escolha de pagamento

---
+++ src/pages/payment/payment.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PedidoDTO } from '../../models/pedido.dto';
+
+@IonicPage()
+@Component({
+  selector: 'page-payment',
+  templateUrl: 'payment.html',
+})
+export class PaymentPage {
+
+  pedido: PedidoDTO;// Recebido pela pagina que irei receber o Endereço
+
+  parcelas: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  formGroup: FormGroup; //Formulario para controlar os dados
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {//Montar o controlador do formulario
+
+    this.pedido = this.navParams.get('pedido');
+
+    this.formGroup = this.formBuilder.group({
+      numeroDeParcelas: [1, Validators.required],
+      "@type": ["pagamentoComCartao", Validators.required]// Tem que estar igual lá no backand
+    });
+  }
+
+  nextPage() {
+    this.pedido.pagamento = this.formGroup.value;
+    console.log(this.pedido);
+  }
+}
