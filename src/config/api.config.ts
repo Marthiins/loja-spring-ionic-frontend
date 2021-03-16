e32b32cadc6e7a8d804d59ada5caface79eeccba
Marthiins <sergio.marthiins@gmail.com>
2021-03-15 22:05:44 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {//API_CONFIG  é meu objeto
-  baseUrl: "http://localhost:8080", //Url do meu springboot
+ //baseUrl: "http://localhost:8080", //Url do meu springboot
+ baseUrl: "https://marthins-spring-ionic.herokuapp.com",
   bucketBaseUrl: "https://marthins-curso-spring-ionic.s3-sa-east-1.amazonaws.com"
 
 }
